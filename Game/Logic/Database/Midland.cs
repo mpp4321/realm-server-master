ac91695bbd6c6ef5858f1fd7e96c7e0567b2e432
Fortune <yggdrasiljunk@gmail.com>
2021-09-19 16:31:48 -0500

    firestaff added

--- Game/Logic/Database/Midland.cs
+++ Game/Logic/Database/Midland.cs
@@ -660,7 +660,8 @@ namespace RotMG.Game.Logic.Database
                 new TierLoot(2, LootType.Ability, 0.3f),
                 new TierLoot(3, LootType.Ability, 0.01f),
                 new ItemLoot("Health Potion", 0.3f),
-                new ItemLoot("Magic Potion", 0.1f)
+                new ItemLoot("Magic Potion", 0.1f),
+                new ItemLoot("Dragonfire", 0.025f)
             );
             db.Init("Drake Baby",
                 new State("base",
