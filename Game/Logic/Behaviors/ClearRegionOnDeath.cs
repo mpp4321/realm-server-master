2e52baf8572fd45eed446bf02632147a6556b76d
mpp4321 <47093830+mpp4321@users.noreply.github.com>
2021-09-30 20:49:56 -0400

    Clear region

---
+++ Game/Logic/Behaviors/ClearRegionOnDeath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RotMG.Game.Logic.Behaviors
+{
+    class ClearRegionOnDeath : Behavior
+    {
+
+        private readonly Region _region;
+        public ClearRegionOnDeath(Region region)
+        {
+            _region = region;
+        }
+
+        public override void Death(Entity host)
+        {
+            foreach(var point in host.Parent.GetAllRegion(_region))
+            {
+                host.Parent.RemoveStatic(point.X, point.Y);
+            }
+        }
+
+    }
+}
